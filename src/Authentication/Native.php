6a41f51199689256b0df0ee5f274ab446b912415
兰林 <lanlin1987@gmail.com>
2018-11-22 15:22:48 +0800

    STAM-2460

--- src/Authentication/Native.php
+++ src/Authentication/Native.php
@@ -2,6 +2,8 @@
 
 use Nylas\Utilities\API;
 use Nylas\Utilities\Request;
+use Nylas\Utilities\Validate as V;
+use Nylas\Exceptions\NylasException;
 
 /**
  * ----------------------------------------------------------------------------------
@@ -24,6 +26,16 @@ class Native
     // ------------------------------------------------------------------------------
 
     /**
+     * @var array
+     */
+    private $providers =
+    [
+        'gmail', 'yahoo', 'exchange', 'outlook', 'imap', 'icloud', 'hotmail', 'aol'
+    ];
+
+    // ------------------------------------------------------------------------------
+
+    /**
      * Hosted constructor.
      */
     public function __construct()
@@ -33,21 +45,131 @@ class Native
 
     // ------------------------------------------------------------------------------
 
-    public function connectAuthorize()
+    public function connectToken()
     {
-        $params =
-        [
 
-        ];
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
+     * connect authorize
+     *
+     * @param array $params
+     * @return mixed
+     * @throws \Nylas\Exceptions\NylasException
+     */
+    public function connectAuthorize(array $params)
+    {
+        $setting = $this->settingsRules($params);
+
+        $rules = V::keySet(
+            V::key('name', V::stringType()::notEmpty()),
+            V::key('settings', $setting),
+            V::key('provider', V::in($this->providers)),
+            V::key('client_id', V::stringType()::notEmpty()),
+            V::key('email_address', V::email()),
+            V::key('reauth_account_id', V::stringType()::notEmpty(), false)
+        );
+
+        if (!$rules->validate($params))
+        {
+            throw new NylasException('invalid params');
+        }
 
         return $this->request->setFormParams($params)->post(API::LIST['connectAuthorize']);
     }
 
     // ------------------------------------------------------------------------------
 
-    public function connectToken()
+    /**
+     * validate settings params
+     *
+     * @param array $params
+     * @return \Respect\Validation\Validator
+     */
+    private function settingsRules(array $params)
     {
+        $provider = $params['provider'] ?? 'imap';
 
+        switch ($provider)
+        {
+            case 'aol':
+            case 'yahoo':
+            case 'icloud':
+            case 'hotmail': return $this->knownProviderRule();
+
+            case 'imap':     return $this->imapProviderRule();
+            case 'gmail':    return $this->gmailProviderRule();
+            case 'exchange': return $this->exchangeProviderRule();
+
+            default: return $this->imapProviderRule();
+        }
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
+     * @return \Respect\Validation\Validator
+     */
+    private function knownProviderRule()
+    {
+        return V::keySet(V::key('password', V::stringType()::notEmpty()));
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
+     * gmail provider rule
+     *
+     * @return \Respect\Validation\Validator
+     */
+    private function gmailProviderRule()
+    {
+        return V::keySet(
+            V::key('google_client_id', V::stringType()::notEmpty()),
+            V::key('google_client_secret', V::stringType()::notEmpty()),
+            V::key('google_refresh_token', V::stringType()::notEmpty())
+        );
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
+     * exchange provider rules
+     *
+     * @return \Respect\Validation\Validator
+     */
+    private function exchangeProviderRule()
+    {
+        return V::keySet(
+            V::key('username', V::stringType()::notEmpty()),
+            V::key('password', V::stringType()::notEmpty()),
+            V::key('eas_server_host', V::stringType()::notEmpty())
+        );
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
+     *
+     *
+     * @return \Respect\Validation\Validator
+     */
+    private function imapProviderRule()
+    {
+        return V::keySet(
+            V::key('imap_host', V::stringType()::notEmpty()),
+            V::key('imap_port', V::stringType()::notEmpty()),
+            V::key('imap_username', V::stringType()::notEmpty()),
+            V::key('imap_password', V::stringType()::notEmpty()),
+
+            V::key('smtp_host', V::stringType()::notEmpty()),
+            V::key('smtp_port', V::stringType()::notEmpty()),
+            V::key('smtp_username', V::stringType()::notEmpty()),
+            V::key('smtp_password', V::stringType()::notEmpty()),
+            V::key('ssl_required', V::boolType())
+        );
     }
 
     // ------------------------------------------------------------------------------
