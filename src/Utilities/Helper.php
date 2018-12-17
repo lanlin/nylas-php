3486cf41fb15610ad34bfb0201e5d75be6ff3f32
兰林 <lanlin1987@gmail.com>
2018-12-17 14:45:05 +0800

    STAM-2475

--- src/Utilities/Helper.php
+++ src/Utilities/Helper.php
@@ -14,12 +14,25 @@ class Helper
     // ------------------------------------------------------------------------------
 
     /**
+     * convert assoc array to multi
+     *
+     * @param array $arr
+     * @return array
+     */
+    public static function arrayToMulti(array $arr) : array
+    {
+        return self::isAssoc($arr) ? [$arr] : $arr;
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
      * check if assoc array
      *
      * @param array $arr
      * @return bool
      */
-    public static function isAssoc(array $arr)
+    public static function isAssoc(array $arr) : bool
     {
         foreach ($arr as $key => $value)
         {
@@ -37,7 +50,7 @@ class Helper
      * @param $foo
      * @return array
      */
-    public static function fooToArray($foo)
+    public static function fooToArray($foo) : array
     {
         if ($foo === null) { return []; }
 
@@ -49,14 +62,55 @@ class Helper
     // ------------------------------------------------------------------------------
 
     /**
-     * convert assoc array to multi
+     * pickup element from inside array
      *
-     * @param array $arr
+     * @param  array  $data
+     * @param  string $key
      * @return array
      */
-    public static function arrayToMulti(array $arr)
+    public static function generateArray(array $data, string $key = 'id') : array
     {
-        return self::isAssoc($arr) ? [$arr] : $arr;
+        $temp = [];
+
+        if (!count($data)) { return $temp; }
+
+        foreach ($data as $val)
+        {
+            if (empty($val[$key])) { continue; }
+
+            $temp[] = $val[$key];
+        }
+
+        return $temp;
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
+     * concat pool return infos
+     *
+     * @param array $params
+     * @param array $pools
+     * @return array
+     */
+    public static function concatPoolInfos(array $params, array $pools) : array
+    {
+        $data = [];
+
+        foreach ($params as $index => $id)
+        {
+            $item = ['id' => $id];
+
+            // check if get errors
+            if (isset($pools[$index]['error']))
+            {
+                $item = array_merge($item, $pools[$index]);
+            }
+
+            $data[$id] = $item;
+        }
+
+        return $data;
     }
 
     // ------------------------------------------------------------------------------
