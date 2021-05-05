ee75e1a3e21ae1a5a2b1a91c760bc337f9518044
jgriffin <jeremy.griffin@movehq.com>
2021-05-05 17:00:57 -0400

    test: add job status tests like the events tests
    This job status is based on a test account I'm using now, so it's as
    good as the other tests.
    
      New file:   tests/JobStatusTest.php

---
+++ tests/JobStatusTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Nylas\Tests;
+
+use Exception;
+
+/**
+ * ----------------------------------------------------------------------------------
+ * JobStatus Test
+ * ----------------------------------------------------------------------------------
+ *
+ * @update lanlin
+ * @change 2021/05/05
+ *
+ * @internal
+ */
+class JobStatusTest extends AbsCase
+{
+    // ------------------------------------------------------------------------------
+
+    public function testGetJobStatusList(): void
+    {
+        $data = $this->client->JobStatuses()->JobStatus()->getJobStatusList();
+
+        $this->assertTrue(\count($data) > 0);
+    }
+
+    // ------------------------------------------------------------------------------
+
+    public function testGetJobStatus(): void
+    {
+        $params = ['job_status_id' => '2k6yf5y2660mqjzwbg3aig92i'];
+
+        $data = $this->client->JobStatuses()->JobStatus()->getJobStatus($params);
+
+        $this->assertArrayHasKey('job_status_id', $data);
+    }
+
+    // ------------------------------------------------------------------------------
+}
